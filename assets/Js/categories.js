c7e40ba394c455b1c06e5553ad603aabff362418
quimack <75865290+quimack@users.noreply.github.com>
2021-08-10 21:46:45 -0300

    add categories to table

--- assets/Js/categories.js
+++ assets/Js/categories.js
@@ -1,4 +1,34 @@
 var storage = getLocalStorage();
+var categoriesTable = document.getElementById('categoriesTable');
+var loadCategories = function () {
+    console.log('entró');
+    categoriesTable.innerHTML = "";
+    // let storage: LocalStorage = getLocalStorage();
+    var categories = storage.categories;
+    for (var _i = 0, categories_1 = categories; _i < categories_1.length; _i++) {
+        var category = categories_1[_i];
+        var tr = document.createElement('tr');
+        var tdName = document.createElement('td');
+        var tdEdit = document.createElement('td');
+        var tdDelete = document.createElement('td');
+        var btnEdit = document.createElement('button');
+        var btnDelete = document.createElement('button');
+        btnEdit.dataset.id = "" + category.id;
+        btnDelete.dataset.id = "" + category.id;
+        var textName = document.createTextNode(category.name);
+        var textEdit = document.createTextNode('Edit');
+        var textDelete = document.createTextNode('Delete');
+        tdName.appendChild(textName);
+        btnEdit.appendChild(textEdit);
+        btnDelete.appendChild(textDelete);
+        tdEdit.appendChild(btnEdit);
+        tdDelete.appendChild(btnDelete);
+        tr.appendChild(tdName);
+        tr.appendChild(tdEdit);
+        tr.appendChild(tdDelete);
+        categoriesTable.appendChild(tr);
+    }
+};
 var formNewCategory = document.getElementById("formNewCategory");
 var addNewCategory = function (e) {
     e.preventDefault();
@@ -10,6 +40,7 @@ var addNewCategory = function (e) {
     };
     storage.categories.push(newCategory);
     localStorage.setItem('piggy-storage', JSON.stringify(storage));
+    loadCategories();
     console.log(storage);
 };
 formNewCategory.addEventListener('submit', addNewCategory);
--- assets/Js/categories.ts
+++ assets/Js/categories.ts
@@ -1,8 +1,45 @@
 
 let storage: LocalStorage = getLocalStorage();
+
+const categoriesTable = document.getElementById('categoriesTable');
+
+const loadCategories = () => {
+    console.log('entró');
+    categoriesTable.innerHTML = "";
+   // let storage: LocalStorage = getLocalStorage();
+    let categories = storage.categories;
+    
+
+    for(const category of categories){
+        const tr = document.createElement('tr');
+        const tdName = document.createElement('td');
+        const tdEdit = document.createElement('td');
+        const tdDelete = document.createElement('td');
+        const btnEdit = document.createElement('button');
+        const btnDelete = document.createElement('button');
+        btnEdit.dataset.id = `${category.id}`;
+        btnDelete.dataset.id = `${category.id}`;
+        const textName = document.createTextNode(category.name) 
+        const textEdit = document.createTextNode('Edit');
+        const textDelete = document.createTextNode('Delete');
+        
+        tdName.appendChild(textName);
+        btnEdit.appendChild(textEdit);
+        btnDelete.appendChild(textDelete);
+        tdEdit.appendChild(btnEdit);
+        tdDelete.appendChild(btnDelete)
+        tr.appendChild(tdName);
+        tr.appendChild(tdEdit);
+        tr.appendChild(tdDelete);
+        
+        categoriesTable.appendChild(tr);
+    }
+
+}
 const formNewCategory = document.getElementById("formNewCategory");
 
 const addNewCategory = (e) =>{
+
     e.preventDefault();
     const form = e.target;
     const newCategoryName: string = form.name.value;
@@ -12,7 +49,9 @@ const addNewCategory = (e) =>{
     }
     storage.categories.push(newCategory);
     localStorage.setItem('piggy-storage', JSON.stringify(storage));
+    loadCategories();
     console.log(storage);
 }
 
 formNewCategory.addEventListener('submit', addNewCategory);
+
