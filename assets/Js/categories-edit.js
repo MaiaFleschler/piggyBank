38542716626aac4f4f08324eaf826b53b5f69c3f
Maia <maiafleschler@gmail.com>
2021-08-14 18:42:52 -0300

    Add value while editing

--- assets/Js/categories-edit.js
+++ assets/Js/categories-edit.js
@@ -1,5 +1,15 @@
 var param = new URLSearchParams(window.location.search);
 var formEditCategory = document.getElementById('formEditCategory');
+var inpName = document.getElementById('inpName');
+var showCurrValues = function () {
+    var storage = getLocalStorage();
+    storage.categories.forEach(function (element) {
+        if (element.id == param.get("id")) {
+            inpName.value = element.name;
+        }
+    });
+};
+showCurrValues();
 var editCategory = function (e) {
     var storage = getLocalStorage();
     var form = e.target;
